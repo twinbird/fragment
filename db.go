a2df76cab927abe7bba077e9f725b630880cdf49
twinbird <ixa2063@gmail.com>
2017-07-09 21:18:31 -0700

    add expired func

--- db.go
+++ db.go
@@ -3,14 +3,16 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 type storeValue struct {
-	key     []byte
-	flags   int
-	exptime int
-	bytes   int
-	data    []byte
+	key        []byte
+	flags      int
+	exptime    int
+	bytes      int
+	data       []byte
+	storedTime time.Time
 }
 
 func (sv *storeValue) toString() string {
@@ -24,9 +26,23 @@ type inmemoryDB struct {
 	bucket map[string]*storeValue
 }
 
+func isExpired(v *storeValue) bool {
+	if v.exptime == 0 {
+		return false
+	}
+	exptime := time.Duration(v.exptime)
+	t := v.storedTime.Add(exptime * time.Second)
+	if t.After(time.Now()) == true {
+		return true
+	} else {
+		return false
+	}
+}
+
 func (db *inmemoryDB) set(key []byte, value *storeValue) error {
 	db.mutex.Lock()
 	defer db.mutex.Unlock()
+	value.storedTime = time.Now()
 	db.bucket[string(key)] = value
 
 	return nil
@@ -36,6 +52,13 @@ func (db *inmemoryDB) get(key []byte) (*storeValue, error) {
 	db.mutex.RLock()
 	defer db.mutex.RUnlock()
 	v := db.bucket[string(key)]
+	if v == nil {
+		return nil, nil
+	}
+
+	if isExpired(v) == true {
+		return nil, nil
+	}
 
 	return v, nil
 }
@@ -43,10 +66,15 @@ func (db *inmemoryDB) get(key []byte) (*storeValue, error) {
 func (db *inmemoryDB) add(key []byte, value *storeValue) (bool, error) {
 	db.mutex.Lock()
 	defer db.mutex.Unlock()
+
+	value.storedTime = time.Now()
 	v := db.bucket[string(key)]
 	if v != nil {
-		return false, nil
+		if isExpired(v) == false {
+			return false, nil
+		}
 	}
+
 	db.bucket[string(key)] = value
 
 	return true, nil
@@ -55,10 +83,17 @@ func (db *inmemoryDB) add(key []byte, value *storeValue) (bool, error) {
 func (db *inmemoryDB) replace(key []byte, value *storeValue) (bool, error) {
 	db.mutex.Lock()
 	defer db.mutex.Unlock()
+
+	value.storedTime = time.Now()
 	v := db.bucket[string(key)]
 	if v == nil {
 		return false, nil
 	}
+
+	if isExpired(v) == true {
+		return false, nil
+	}
+
 	db.bucket[string(key)] = value
 
 	return true, nil
@@ -71,6 +106,11 @@ func (db *inmemoryDB) delete(key []byte) (bool, error) {
 	if v == nil {
 		return false, nil
 	}
+
+	if isExpired(v) == true {
+		return false, nil
+	}
+
 	db.bucket[string(key)] = nil
 
 	return true, nil
